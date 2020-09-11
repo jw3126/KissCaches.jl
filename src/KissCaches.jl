9dccd48413596d7f63de26eba5fcfd41661d33c2
Jan Weidner <jw3126@gmail.com>
2020-09-11 09:33:49 +0200

    init

--- src/KissCaches.jl
+++ src/KissCaches.jl
@@ -1,5 +1,64 @@
 module KissCaches
+export cached, @cached, iscached
 
-# Write your package code here.
+function cached(storage, f, args...; kw...)
+    k = makekey(storage, f, args...; kw...)
+    get!(storage, k) do
+        @debug """
+        Adding to cache:
+        f = $f
+        args = $args
+        kw = $kw
+        k = $k
+        cache = $cache
+        """
+        f(args...; kw...)
+    end
+end
+# makekey(storage, f, args...; kw...) = Base.hash((f, args, kw))
+makekey(storage, f, args...; kw...) = (f, args, kw.data)
+
+function iscached(storage, f, args...; kw...)
+    k = makekey(storage, f, args...; kw...)
+    haskey(storage, k)
+end
+
+function deletecached!(storage, f, args...; kw...)
+    k = makekey(storage, f, args...; kw...)
+    delete!(storage, k)
+end
+
+const GLOBAL_CACHE = Dict()
+
+macro cached(ex)
+    c = GLOBAL_CACHE
+    esc(cachedmacro(c, ex))
+end
+
+macro cached(cache, ex)
+    esc(cachedmacro(cache, ex))
+end
+
+function cachedmacro(cache, ex)
+    if Meta.isexpr(ex, :call)
+        if length(ex.args) >= 2 && Meta.isexpr(ex.args[2], :parameters)
+            Expr(:call, cached, ex.args[2], cache, ex.args[1], ex.args[3:end]...)
+        else
+            Expr(:call, cached, cache, ex.args...)
+        end
+    else
+        ex
+    end
+end
+
+# function apply_cache_to_all_calls(cache, expr)
+#     MacroTools.postwalk(expr) do ex
+#         if Meta.isexpr(ex, :call)
+#             Expr(:call, cached, cache, ex.args...)
+#         else
+#             ex
+#         end
+#     end
+# end Write your package code here.
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,48 @@
 using KissCaches
+using KissCaches: deletecached!
 using Test
 
-@testset "KissCaches.jl" begin
-    # Write your tests here.
+
+function self(args...; kw...)
+    (args, kw)
+end
+
+function test_cached(cache, f, args...; kw...)
+    @test !iscached(cache, f, args...; kw...)
+    @cached cache f(args...; kw...)
+    @test iscached(cache, f, args...; kw...)
+    deletecached!(cache, f, args...; kw...)
+    @test !iscached(cache, f, args...; kw...)
+end
+
+@testset "test" begin
+    cache = Dict()
+    @test !iscached(cache, sin, 1)
+    @cached cache sin(1)
+    @test iscached(cache, sin, 1)
+    empty!(cache)
+    @test !iscached(cache, sin, 1)
+    @test !iscached(cache, cos, 1)
+
+    test_cached(Dict(), sin, 1)
+    test_cached(Dict(), self)
+    test_cached(Dict(), self, 1)
+    test_cached(Dict(), self, x=:x)
+    test_cached(Dict(), self, 1, x=1)
+    test_cached(Dict(), self, 1, x=1, y="hi", z=["a", Any])
+    test_cached(Dict(), self, 1, x=1, y="hi", z=["a", Any])
+end
+
+@testset "GLOBAL_CACHE" begin
+    @test !iscached(KissCaches.GLOBAL_CACHE, self)
+    @cached self()
+    @test iscached(KissCaches.GLOBAL_CACHE, self)
+    for (args, kw) in [
+            ((1,), NamedTuple()),
+            ((), (a=1,)),
+        ]
+        @test !iscached(KissCaches.GLOBAL_CACHE, self, args...; kw...)
+        @cached self(args...; kw...)
+        @test iscached(KissCaches.GLOBAL_CACHE, self, args...; kw...)
+    end
 end
